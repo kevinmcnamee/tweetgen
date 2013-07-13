dedabb2df89c90f8d9972a458f21a05a685fd50f
kevinmcnamee <mcnameekm@gmail.com>
2013-07-13 17:12:15 -0400

    css

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, 'k9boEuIJ1Z6ua8O9DYmXRA', 'PM4Eyun0ApD7Y5vQc9RjCOxjCF8WUTOnaek2jGDJDA'
 end
